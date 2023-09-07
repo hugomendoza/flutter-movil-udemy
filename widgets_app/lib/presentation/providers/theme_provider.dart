fafb1eb9d91b7d64fb31d493bc84fe4ad5e57d79
Hugo <hugo_mendoza_s@outlook.com>
2023-09-07 08:56:27 -0500

    feat: advance ricerpod section

---
+++ widgets_app/lib/presentation/providers/theme_provider.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:widgets_app/config/theme/app_theme.dart';
+
+final colorListProvider = Provider((ref) => colorList);
+final isDarkModeProvider = StateProvider<bool>((ref) => false);
+final selectedColorProvider = StateProvider<int>((ref) => 0);
--- widgets_app/lib/presentation/screens/counter/counter_screen.dart
+++ widgets_app/lib/presentation/screens/counter/counter_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widgets_app/presentation/providers/counter_provider.dart';
+import 'package:widgets_app/presentation/providers/theme_provider.dart';
 
 class CounterScreen extends ConsumerWidget {
 
@@ -12,17 +13,30 @@ class CounterScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
 
     final int clickCounter = ref.watch(counterProvider);
+    final bool isDarkMode = ref.watch(isDarkModeProvider);
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Counter Screen'),
+        actions: [
+          IconButton(
+            icon: Icon( isDarkMode ? Icons.dark_mode_outlined : Icons.light_mode_outlined),
+            onPressed: () {
+              ref.read(isDarkModeProvider.notifier)
+                .update((isDarkmode) => !isDarkmode);
+            },
+          )
+        ],
       ),
       body: Center(
         child: Text('Valor: $clickCounter', style: Theme.of(context).textTheme.titleLarge ),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () {
+          ref.read(counterProvider.notifier).state++;
+          // ref.read(counterProvider.notifier).update((state) => state + 1);
+        },
       ) 
     );
   }
