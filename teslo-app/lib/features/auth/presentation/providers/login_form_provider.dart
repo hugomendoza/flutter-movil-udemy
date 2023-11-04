93f98384a837ec26848e1e26f76fd47bef907578
Hugo <hugo_mendoza_s@outlook.com>
2023-11-04 13:19:27 -0500

    feat: finish section login

--- teslo-app/lib/features/auth/presentation/providers/login_form_provider.dart
+++ teslo-app/lib/features/auth/presentation/providers/login_form_provider.dart
@@ -1,10 +1,17 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:formz/formz.dart';
 
+import 'package:teslo_shop/features/auth/presentation/providers/auth_provider.dart';
+
 import 'package:teslo_shop/features/shared/shared.dart';
 
 final loginFormProvider = StateNotifierProvider.autoDispose<LoginFormNotifier, LoginFormState>((ref) {
-  return LoginFormNotifier();
+
+  final loginUserCallBack =  ref.watch(authProvider.notifier).loginUser;
+
+  return LoginFormNotifier(
+    loginUserCallBack: loginUserCallBack
+  );
 });
 
 
@@ -53,7 +60,12 @@ class LoginFormState {
 }
 
 class LoginFormNotifier extends StateNotifier<LoginFormState> {
-  LoginFormNotifier(): super( LoginFormState() );
+
+  final Function(String, String) loginUserCallBack;
+
+  LoginFormNotifier({
+    required this.loginUserCallBack
+  }): super( LoginFormState() );
 
   onEmailChanged(String value) {
     final newEmail = Email.dirty(value);
@@ -71,10 +83,10 @@ class LoginFormNotifier extends StateNotifier<LoginFormState> {
     );
   }
 
-  onFormSubmit() {
+  onFormSubmit() async {
     _touchedEveryField();
     if( !state.isValid ) return;
-    print(state);
+    await loginUserCallBack(state.email.value, state.password.value);
   }
 
   _touchedEveryField() {
