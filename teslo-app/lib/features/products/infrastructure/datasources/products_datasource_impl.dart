568f44dd9c5678109e74de93fc8f4b83ef0d5b6c
Hugo <hugo_mendoza_s@outlook.com>
2023-11-18 14:03:43 -0500

    feat: section update products

--- teslo-app/lib/features/products/infrastructure/datasources/products_datasource_impl.dart
+++ teslo-app/lib/features/products/infrastructure/datasources/products_datasource_impl.dart
@@ -1,6 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:teslo_shop/config/config.dart';
+
 import 'package:teslo_shop/features/products/domain/domain.dart';
+import '../errors/product_errors.dart';
 import '../mapper/product_mapper.dart';
 
 class ProductsDatasourceImpl implements ProductsDatasource {
@@ -27,9 +29,20 @@ class ProductsDatasourceImpl implements ProductsDatasource {
   }
 
   @override
-  Future<Product> getProductById(String id) {
-    // TODO: implement getProductById
-    throw UnimplementedError();
+  Future<Product> getProductById(String id) async {
+    
+    try {
+      
+      final response = await dio.get('/products/$id');
+      final product = ProductMapper.jsonToEntity(response.data);
+      return product;
+
+    } on DioException catch (e) {
+      if(e.response?.statusCode == 404) throw ProductNotFound();
+      throw Exception();
+    } catch (e) {
+      throw Exception();
+    }
   }
 
   @override
